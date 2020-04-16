dbb726b6d4e8314df130f1fe30a5e346a93a3b69
Joaquin Diaz <joacod@gmail.com>
2020-04-16 17:36:23 +0200

    Create and print a struct

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"playground/models"
+)
 
 func main() {
 	fmt.Println("Hello Go!")
+
+	user := models.User{
+		ID:        2,
+		FirstName: "Mr",
+		LastName:  "Spock",
+	}
+
+	fmt.Println(user)
 }
